f750cc8aef4fd1d1f10e0806e726c6495aec3989
Aryan Thapak <aryanthapak8@gmail.com>
2026-01-19 23:03:28 +0530

    feat:added auth implemenations

--- backend/src/main/java/com/example/backend/service/AuthServiceImp.java
+++ backend/src/main/java/com/example/backend/service/AuthServiceImp.java
@@ -1,5 +1,34 @@
 package com.example.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.dto.AuthDto;
+import com.example.backend.model.Student;
+import com.example.backend.repository.AdminRepository;
+import com.example.backend.repository.StudentRepository;
+
+@Service
 public class AuthServiceImp implements AuthServiceInterface{
+	@Autowired
+	StudentRepository studentRepository;
 	
+	@Autowired
+	AdminRepository adminRepository;
+	
+	@Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+		
+	@Override
+	public StudentDto registerStudent(Student student) {
+		if(studentRepository.findById(student.getId()).isPresent()) {
+			return null;
+		}
+		student.setPassword(bCryptPasswordEncoder.encode(student.getPassword()));
+		Student createdStudent=studentRepository.save(student);
+		
+		return StudentDtoMapper.to
+	}
 }
